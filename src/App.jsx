5560b4301a18f8ca97795f7ca6fb5ebae9d8ffb1
Yana GttG <y.gottselig@gmail.com>
2025-07-02 20:25:40 +0200

    Add and integrate About component, fix import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+
+import About from './components/About';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <About />    </>
   )
 }
 
