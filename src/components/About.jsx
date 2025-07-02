824fb744c0ebb78eec39a9a19f7e826c3fc9f526
Yana GttG <y.gottselig@gmail.com>
2025-07-02 19:41:42 +0200

    Initial commit: React Vite app

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about-container">
+      <h2>Обо мне</h2>
+      <p>Привет! Меня зовут [Яна].</p>
+      <p>Я начинающий разработчик и это мой первый react проект.</p>
+      <p>Это тестовая страница.</p> 
+    </div>
+  );
+}
+
+export default About;
