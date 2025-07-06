e9be59b027697ade2e497d0fe2499dab51d01007
Yana GttG <y.gottselig@gmail.com>
2025-07-06 21:38:45 +0200

    Добавила темную светлую тему и исправила синтаксические ошибки в App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react' 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,18 +9,28 @@ import { ThemeSwitch } from './components/ThemeSwitch/ThemeSwitch';
 import { Counter } from './components/Counter/Counter.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [currentTheme, setCurrentTheme] = useState('dark'); 
 
   const handleIncrement = () => {
     setCount(prevCount => prevCount + 1);
-  }
+  };
+
+  const handleThemeChange = (newTheme) => {
+    setCurrentTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = currentTheme; 
+  }, [currentTheme]); 
 
   return (
-    <>
+    <div className={currentTheme}>
       <About />
-      <ThemeSwitch></ThemeSwitch>
+      <ThemeSwitch currentTheme={currentTheme} onThemeChange={handleThemeChange} />
       <Counter count={count} onIncrement={handleIncrement} />
-         </>
+    </div> 
   )
 }
 
